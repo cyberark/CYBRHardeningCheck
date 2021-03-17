2f66a597a5f4b0d3ae48ddaa76592aae75200c42
Assaf Miron <AssafMiron@users.noreply.github.com>
2021-03-17 18:45:26 +0200

    Added new hardening check for Privilege Cloud secure tunnel

--- CYBRHardeningCheck/CPM/CPMHardeningSteps.psm1
+++ CYBRHardeningCheck/CPM/CPMHardeningSteps.psm1
@@ -65,9 +65,8 @@ Function CPM_Password_Manager_Services_LocalUser
 
 			if((Compare-UserPermissions -path $cpmServicesPath -identity $(Get-LocalSystem) -rights "FullControl" -outStatus ([ref]$myRef)) -ne "Good")
 			{
-
-            $CPMFolderLocalAdmins = $false
-            $res = "Warning"
+	            $CPMFolderLocalAdmins = $false
+    	        $res = "Warning"
 			}
             $tmpStatus += "<li>" + $myRef.Value + "</li>"
 
