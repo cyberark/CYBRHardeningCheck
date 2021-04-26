b762908c69583a95a753bca2059003fd4de0c4f0
Assaf Miron <AssafMiron@users.noreply.github.com>
2021-04-26 10:04:35 +0300

    Resolves #38, Resolves #46

--- CYBRHardeningCheck/Main.ps1
+++ CYBRHardeningCheck/Main.ps1
@@ -347,7 +347,7 @@ If($(Test-CurrentUserLocalAdmin) -eq $false)
 }
 
 # Check if relevant files are blocked
-If($null -eq $(Get-ChildItem -Path $ScriptLocation -Include ('*.ps1','*.psm1','*.dll') -Recurse | Get-Item -Stream “Zone.Identifier” -ErrorAction SilentlyContinue))
+If($null -ne $(Get-ChildItem -Path $ScriptLocation -Include ('*.ps1','*.psm1','*.dll') -Recurse | Get-Item -Stream “Zone.Identifier” -ErrorAction SilentlyContinue))
 {
 	Write-LogMessage -Type Error -Msg "Some files are marked as blocked"
 	$command = "Get-ChildItem -Path $ScriptLocation -Recurse | Unblock-File"
